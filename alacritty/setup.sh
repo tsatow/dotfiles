8adc41aba3ef9092ff7fad9e9352c0072deb45b9
tsatow <t.satoooooooooooo@gmail.com>
2024-02-28 10:34:39 +0900

    migrate alacretty config yaml files to toml

--- alacritty/setup.sh
+++ alacritty/setup.sh
@@ -1,6 +1,6 @@
 if [ ! -d ${HOME}/.config/alacritty ]; then mkdir -p ${HOME}/.config/alacritty; fi
-ln -sf ${PWD}/alacritty/alacritty.yml ${HOME}/.config/alacritty/alacritty.yml
-ln -sf ${PWD}/alacritty/$(uname).yml ${HOME}/.config/alacritty/os-settings.yml
+ln -sf ${PWD}/alacritty/alacritty.toml ${HOME}/.config/alacritty/alacritty.toml
+ln -sf ${PWD}/alacritty/$(uname).toml ${HOME}/.config/alacritty/os-settings.toml
 
 # 設置ファイルのサンプルをダウンロード
 wget -q "https://github.com/alacritty/alacritty/releases/download/v$(alacritty --version | cut -d ' ' -f 2)/alacritty.yml" -O ${PWD}/alacritty/alacritty.yml.example
