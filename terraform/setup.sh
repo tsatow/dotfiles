3f50667f52d05850644b6c868fd33d005461f595
tsatow <t.satoooooooooooo@gmail.com>
2024-04-05 11:25:12 +0900

    update terraform

--- terraform/setup.sh
+++ terraform/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 asdf plugin add terraform # https://github.com/asdf-community/asdf-hashicorp.git
-asdf install terraform 1.2.1
-asdf global terraform 1.2.1
+asdf install terraform 1.7.5
+asdf global terraform 1.7.5
