8e7c849088296d979ab05230050f7924aeb353ff
tsatow <t.satoooooooooooo@gmail.com>
2026-02-27 20:36:46 +0900

    fix

--- ghostty/setup.sh
+++ ghostty/setup.sh
@@ -1,2 +1,2 @@
-mkdir -p ${HOME}/config/ghostty
-ln -sf ${PWD}/ghostty/config ${HOME}/config/ghostty/config
+mkdir -p ${HOME}/.config/ghostty
+ln -sf ${PWD}/ghostty/config ${HOME}/.config/ghostty/config
