26f36a7aa13892a67d6488cb048a3617a134ef01
tsatow <t.satoooooooooooo@gmail.com>
2026-08-13 10:29:33 +0900

    Zedに移行準備

--- zed/setup.sh
+++ zed/setup.sh
@@ -1,3 +1,5 @@
 if [ ! -d ${HOME}/.config/zed ]; then mkdir -p ${HOME}/.config/zed; fi
 ln -sf ${PWD}/zed/settings.json ${HOME}/.config/zed/settings.json
 ln -sf ${PWD}/zed/keymap.json ${HOME}/.config/zed/keymap.json
+ln -sf ${PWD}/zed/tasks.json ${HOME}/.config/zed/tasks.json
+ln -sf ${PWD}/zed/org-todo ${HOME}/.local/bin/org-todo
