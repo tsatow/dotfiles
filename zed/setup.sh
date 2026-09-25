ab73f024d3fb8069ae91d30a61a93049dec311cd
tsatow <t.satoooooooooooo@gmail.com>
2026-09-25 12:44:10 +0900

    zedのスクリプトの修正

--- zed/setup.sh
+++ zed/setup.sh
@@ -1,5 +1,11 @@
-if [ ! -d ${HOME}/.config/zed ]; then mkdir -p ${HOME}/.config/zed; fi
-ln -sf ${PWD}/zed/settings.json ${HOME}/.config/zed/settings.json
-ln -sf ${PWD}/zed/keymap.json ${HOME}/.config/zed/keymap.json
-ln -sf ${PWD}/zed/tasks.json ${HOME}/.config/zed/tasks.json
-ln -sf ${PWD}/zed/org-todo ${HOME}/.local/bin/org-todo
+#!/bin/bash
+set -eu
+
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+
+mkdir -p ${HOME}/.config/zed
+mkdir -p ${HOME}/.local/bin
+ln -sf "$SCRIPT_DIR/settings.json" ${HOME}/.config/zed/settings.json
+ln -sf "$SCRIPT_DIR/keymap.json" ${HOME}/.config/zed/keymap.json
+ln -sf "$SCRIPT_DIR/tasks.json" ${HOME}/.config/zed/tasks.json
+ln -sf "$SCRIPT_DIR/org-todo" ${HOME}/.local/bin/org-todo
